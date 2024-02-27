ea337ed9fb23258aa110b3a75e11b26081142a4b
Muhammet <Muhammet@DESKTOP-HU703JA>
2024-02-28 01:02:35 +0300

    Proje dosyası ekle.

---
+++ c#proje/Program.cs
@@ -0,0 +1,34 @@
+﻿using c_proje.issürecleri;
+using c_proje.varliklar;
+
+Console.WriteLine("Hello. World!");
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+bool isAuthenticated = true;
+Console.WriteLine(message1);
+
+if (isAuthenticated)
+    Console.WriteLine("Buton-->Hosgeldin Muhammet");
+else
+    Console.WriteLine("Buton-->Sisteme giris yap");
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6" };
+
+for (int i=0; i<loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+Console.WriteLine("kod bitti");
---
+++ c#proje/varliklar/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_proje.varliklar;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+
+}
